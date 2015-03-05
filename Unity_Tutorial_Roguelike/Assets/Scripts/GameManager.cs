6cf40bfb9c07c30138a3189c3160ba26d241c3a4
Túlio Bazan da Silva <tucobz@gmail.com>
2015-03-05 15:48:43 -0300

    Tutorial 9 - Construindo Player Script (7:00min)

--- Unity_Tutorial_Roguelike/Assets/Scripts/GameManager.cs
+++ Unity_Tutorial_Roguelike/Assets/Scripts/GameManager.cs
@@ -7,9 +7,12 @@ public class GameManager : MonoBehaviour
 {
 	
 	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
-	private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
-	private int level = 3;                                  //Current level number, expressed in game as "Day 1". LOG2
-	
+	public BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+	public int level = 3;                                  //Current level number, expressed in game as "Day 1". LOG2
+	public int playerFoodPoints = 100;
+
+	[HideInInspector]public bool playersTurn = true;
+
 	//Awake is always called before any Start functions
 	void Awake()
 	{
@@ -43,7 +46,10 @@ public class GameManager : MonoBehaviour
 		
 	}
 	
-	
+	public void GameOver(){
+
+		enabled = false;
+	}
 	
 	//Update is called every frame.
 	void Update()
